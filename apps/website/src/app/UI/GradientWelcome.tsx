eef2d7362617aabbdb0057b9a7dd00840aaa0d2a
Joe Fleming <joe@joefleming.co.uk>
2025-08-01 20:14:03 +0100

    feat: turborepo, sanity install

--- apps/website/src/app/UI/GradientWelcome.tsx
+++ apps/website/src/app/UI/GradientWelcome.tsx
@@ -1,11 +1,12 @@
 "use client";
 import styled from "styled-components";
 
-export const GradientWelcome = styled.h1`
+export const GradientWelcome = styled.span`
   margin: 40px 0 30px 0;
-  padding: 20px 30px;
+  padding: 20px 0;
   display: inline-block;
   border-radius: 10px;
+  font-size: 32px;
   background-image: linear-gradient(
     125deg,
     #e642ed,
@@ -15,13 +16,17 @@ export const GradientWelcome = styled.h1`
     #4ce0a0
   );
   background-size: 400%;
-  animation: bganimation 15s infinite;
-  color: #fff;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  -webkit-text-fill-color: transparent;
+  animation: textGradientAnimation 15s infinite;
   position: relative;
-  font-size: 32px;
+
   @media (max-width: 400px) {
     font-size: 28px;
   }
+
   figure {
     position: absolute;
     width: 60px;
@@ -31,6 +36,7 @@ export const GradientWelcome = styled.h1`
     margin: 0;
     border-radius: 50%;
     overflow: hidden;
+
     &:before {
       width: 105%;
       height: 105%;
@@ -52,6 +58,7 @@ export const GradientWelcome = styled.h1`
       animation: bganimation 15s infinite;
     }
   }
+
   img {
     border-radius: 50%;
     z-index: 1;
@@ -62,7 +69,20 @@ export const GradientWelcome = styled.h1`
     position: absolute;
     transform: translate(-50%, -50%);
   }
+
   @keyframes bganimation {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  @keyframes textGradientAnimation {
     0% {
       background-position: 0% 50%;
     }

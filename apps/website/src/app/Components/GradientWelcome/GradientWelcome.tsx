d43d3687b5862dbcec18c04e13106a959b3e2b8a
Joe Fleming <joe@joefleming.co.uk>
2025-08-13 19:55:32 +0100

    refactor: remove unused components and styles for cleaner codebase

--- apps/website/src/app/Components/GradientWelcome/GradientWelcome.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./GradientWelcome.module.css";
-import React from "react";
-
-export const GradientWelcome = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => <span className={styles.gradientWelcome}>{children}</span>;
--- apps/website/src/app/Components/Intro/Intro.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Intro.module.css";
-
-interface IntroProps {
-  children: React.ReactNode;
-}
-
-export const Intro = ({ children }: IntroProps) => {
-  return <div className={styles.intro}>{children}</div>;
-};

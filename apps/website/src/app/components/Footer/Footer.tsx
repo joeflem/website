4975c3c47145789085b8db655d8a97125cc2aa4d
Joe Fleming <joe@joefleming.co.uk>
2026-01-18 20:19:37 +0000

    feat: add CurrentYear component for dynamic copyright year in footer

--- apps/website/src/app/components/Footer/Footer.tsx
+++ apps/website/src/app/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import styles from "./Footer.module.css";
 import Social from "@/app/components/Social/Social";
+import CurrentYear from "@/app/components/Footer/CurrentYear";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p className={styles.copyright}>
-        Joe Fleming &copy; {new Date().getFullYear()}
+        Joe Fleming &copy; <CurrentYear />
       </p>
       <Social />
     </footer>
